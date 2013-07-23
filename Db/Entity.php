a47cdeae38e3cde59da558930aaa6b352a409a4a
Stefan Bauer <sbauer@tritendo.de>
2013-07-23 18:41:27 +0200

    Some code corrections, and changed dirty columns to have column name, new value and old value in it

--- Db/Entity.php
+++ Db/Entity.php
@@ -28,7 +28,13 @@ class Entity {
 
 	private $_loaded = false;
 
-
+	/**
+	 * Constructor
+	 *
+	 * @access	public
+	 * @param	null|string $id
+	 * @return	void
+	 */
 	public final function __construct( $id = null ) {
 
 		if( $this->_tableName == null )
@@ -45,14 +51,15 @@ class Entity {
 	/**
 	 * Populate object with values from array
 	 *
-	 * @param array $values
+	 * @access	public
+	 * @param	array $values
 	 */
-	public function fromArray(array $values) {
+	public function fromArray( array $values ) {
 
 		foreach( $values as $fieldName => $value ) {
 
-			$methodName = 'set'.Application::getInstance()->toCamelCase($fieldName, true, '_');
-			$this->$methodName($value);
+			$methodName = 'set'.Application::getInstance()->toCamelCase( $fieldName, true, '_' );
+			$this->$methodName( $value );
 
 		}
 
@@ -64,40 +71,48 @@ class Entity {
 	 * @param $column
 	 * @param $value
 	 */
-	public function setColumnValue($column, $value) {
+	public function setColumnValue( $column, $value ) {
+
+		$this->_dirtyColumns[] = array(
+			'column' => $column,
+			'old_value' => $this->getColumnValue( $column ),
+			'new_value' => $value
+		);
 
 		$this->_columnValues[$column] = $value;
-		$this->_dirtyColumns[] = $column;
 
 	}
 
 	/**
 	 * Get column value
 	 *
-	 * @param $column
-	 * @return null
+	 * @access	public
+	 * @param	string $column
+	 * @return	string|null
 	 */
 	public function getColumnValue($column) {
 
-		return ($this->_columnValues[$column]) ? $this->_columnValues[$column] : null;
+		return array_key_exists( $column, $this->_columnValues ) ? $this->_columnValues[$column] : null;
 
 	}
 
 	/**
 	 * Check if is dirty
 	 *
-	 * @return bool
+	 * @access	public
+	 * @return	bool
 	 */
 	public function isDirty() {
 
-		return false === empty($this->_dirtyColumns);
+		return false === empty( $this->_dirtyColumns );
 
 	}
 
 	/**
 	 * Returns all stored column values
 	 *
-	 * @return array
+	 * @access	public
+	 * @return	array
 	 */
 	public function toArray() {
 
@@ -105,13 +120,20 @@ class Entity {
 
 	}
 
+	/**
+	 * Saves the current row
+	 *
+	 * @access	public
+	 * @return	$this
+	 */
 	public function save() {
 
 		$db = Database::getInstance();
 
-		if($this->_loaded && false === $this->isDirty())
+		if( $this->_loaded && false === $this->isDirty() )
 			return $this;
 
+		// Decide update or insert
 		if( $this->_loaded )
 			$query = "UPDATE `".$this->_tableName."` SET ";
 		else
@@ -119,24 +141,25 @@ class Entity {
 
 		$set = [];
 
-		foreach($this->_dirtyColumns as $columnName) {
+		// Only update dirty columns
+		foreach( $this->_dirtyColumns as $columnName ) {
 
 			// skip primary keys
-			if( in_array($columnName, $this->_primaryKey) )
+			if( in_array( $columnName, $this->_primaryKey ) )
 				continue;
 
 			$value = $this->_columnValues[$columnName];
 
-			$value = is_numeric($value) ? (int)$value : "'".$db->real_escape_string($value)."'";
+			$value = is_numeric( $value ) ? (int)$value : "'".$db->real_escape_string($value)."'";
 			$set[] = '`'.$columnName.'` = ' . $value;
 
 		}
 
-		$query .= join(', ', $set);
+		// Build query
+		$query .= join( ', ', $set );
 
-		if( $this->_loaded ) {
+		if( $this->_loaded )
 			$query .= ' WHERE '. $this->_getWherePart();
-		}
 
 		$db->query($query);
 
@@ -147,25 +170,25 @@ class Entity {
 	/**
 	 * Magic method that supports getters and setters
 	 *
-	 * @param $name
-	 * @param $arguments
-	 * @return null
+	 * @access	public
+	 * @param	$name
+	 * @param	$arguments
+	 * @return	null
 	 */
-	function __call( $name, $arguments ) {
+	public function __call( $name, $arguments ) {
 
 		// convert from getColumnName to get_column_name
 		$underscoredName = strtolower( preg_replace('/([A-Z])/', '_$1', $name ) );
 
-		if(strpos( $underscoredName,'get_' ) === 0) {
+		if( strpos( $underscoredName, 'get_' ) === 0 ) {
 
-			$columnName = str_replace('get_', '', $underscoredName );
-			return $this->getColumnValue($columnName);
+			$columnName = str_replace( 'get_', '', $underscoredName );
+			return $this->getColumnValue( $columnName );
 
-		}
-		elseif(strpos( $underscoredName,'set_' ) === 0) {
+		} elseif( strpos( $underscoredName, 'set_' ) === 0 ) {
 
-			$columnName = str_replace('set_', '', $underscoredName );
-			$this->setColumnValue($columnName, $arguments[0]);
+			$columnName = str_replace( 'set_', '', $underscoredName );
+			$this->setColumnValue( $columnName, $arguments[0] );
 
 		}
 
@@ -175,22 +198,29 @@ class Entity {
 
 	/**
 	 * Gets table name based on class name
+	 *
+	 * @access	private
+	 * @return	void
 	 */
 	private function _detectTableName() {
 
-		$className = get_class($this);
-		$tableName = lcfirst( substr($className, strrpos($className, '\\') + 1) );
-		$normalizedName = strtolower( preg_replace('/([A-Z])/', '_$1', $tableName ) );
+		$className = get_class( $this );
+		$tableName = lcfirst( substr( $className, strrpos( $className, '\\' ) + 1 ) );
+		$normalizedName = strtolower( preg_replace( '/([A-Z])/', '_$1', $tableName ) );
 		$this->_tableName = $normalizedName;
 
 	}
 
 	/**
-	 * @param mixed $id
+	 * Loads and single row
+	 *
+	 * @access	protected
+	 * @param	mixed $id
+	 * @return	void
 	 */
 	protected function _load( $id ) {
 
-		if( !is_array($id) && !empty($id) ) {
+		if( !is_array( $id ) && !empty( $id ) ) {
 			$id = [$id];
 		}
 
@@ -202,15 +232,15 @@ class Entity {
 		$db = Database::getInstance();
 
 		foreach( $this->_primaryKey as $k => $columnName ) {
-			$value = (is_numeric($id[$k])) ? (int)$id[$k] : '\''.$db->real_escape_string($id[$k]).'\'';
+			$value = ( is_numeric( $id[$k] ) ) ? (int)$id[$k] : '\''.$db->real_escape_string( $id[$k] ).'\'';
 			$whereParts[] = '`'.$columnName.'` = '. $value;
 		}
 
-		$whereString = implode(' AND ', $whereParts);
+		$whereString = implode( ' AND ', $whereParts );
 
 		$query .= ' WHERE ' . $whereString;
 		$query .= ' LIMIT 1';
-		$result = $db->query($query);
+		$result = $db->query( $query );
 		$values = $result->fetch_assoc();
 
 		if( $values ) {
@@ -234,7 +264,7 @@ class Entity {
 	private function _getWherePart() {
 
 		if( !$this->_loaded )
-			throw new Database\Exception('Can not get where part from not loaded object');
+			throw new Database\Exception( 'Can not get where part from not loaded object' );
 
 		$db = Database::getInstance();
 
@@ -244,13 +274,13 @@ class Entity {
 		foreach( $this->_primaryKey as $columnName ) {
 
 			$value = $this->_columnValues[$columnName];
-
-			$value = (is_numeric($value)) ? (int)$value : '\''.$db->real_escape_string($value).'\'';
+			$value = ( is_numeric($value) ) ? (int)$value : '\''.$db->real_escape_string($value).'\'';
 
 			$whereParts[] = '`'.$columnName.'` = '. $value;
+
 		}
 
-		return join(' AND ', $whereParts);
+		return join( ' AND ', $whereParts );
 
 	}
 
